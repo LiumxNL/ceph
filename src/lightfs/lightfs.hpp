0a27e873d46f5ad42b7234f99f07cdcd6404691c
Yunchuan Wen <yunchuanwen@ubuntukylin.com>
2014-08-08 15:35:04 +0800

    root inodeno is 0
    Signed-off-by: Yunchuan Wen <yunchuanwen@ubuntukylin.com>

--- src/lightfs/lightfs.hpp
+++ src/lightfs/lightfs.hpp
@@ -20,6 +20,19 @@ namespace lightfs
     static int init_pool(librados::IoCtx& ioctx, int bits);
     int generate(inodeno_t &ino);
   };
+
+  class DirectoryManager
+  {
+  private:
+    librados::IoCtx _ioctx;
+  public:
+    DirectoryManager(const librados::IoCtx &ioctx);
+    static int init_pool(librados::IoCtx &ioctx);
+    int create(inodeno_t pino, bool excl, inode_t &inode);
+    int remove(inodeno_t pino, const std::string &name, inodeno_t ino);
+    int lookup(inodeno_t pino, const std::string &name, inodeno_t &ino);
+    int lookup(inodeno_t pino, inodeno_t ino);
+  };
 };
 
 #endif
